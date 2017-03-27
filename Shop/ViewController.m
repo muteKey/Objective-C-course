3a00bcc71b211de365fad4a300e5f5dd6aecc8b9
Kirill Ushkov <kirillushkov2010@gmail.com>
2017-03-27 09:34:19 +0300

    added uitableview

--- Shop/ViewController.m
+++ Shop/ViewController.m
@@ -9,10 +9,20 @@
 #import "ViewController.h"
 #import "Item.h"
 
-@interface ViewController ()
+// Далее необходимо определить источник данных для нашей таблицы, это можно посмотреть в UITableView.h
+
+// Добавим реализацию протокола UITableViewDatasource в наш класс
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+// добавим аутлет нашей таблицы
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 // объявляем свойство класса для хранения информации о наших товарах
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 @property (nonatomic, strong) NSArray *shop;
+
+// далее добавим изменяемый массив, который будет хранить товары
+@property (nonatomic, strong) NSMutableArray *items;
 @end
 
 @implementation ViewController
@@ -27,6 +37,12 @@
     
     // затем отображаем эти данные
     [self showData];
+    
+    // но сначала надо установить наш класс как источник данных таблицы
+    self.tableView.dataSource = self;
+    
+    // также необходимо установить свойство delegate таблицы наш класс
+    self.tableView.delegate = self;
 }
 
 
@@ -48,10 +64,17 @@
     // далее напишем логику для получения текстовых представлений каждого товара
     NSMutableArray *textRepresentation = [NSMutableArray array];
     
+    // проиницианизируем массив товаров
+    
+    self.items = [[NSMutableArray alloc] init];
+    
     for (NSDictionary *item in self.shop) {
         Item *itemObj = [[Item alloc] initWithDictionary:item];
 
-        if (itemObj) {
+        // теперь все объекты Item будем добавлять в массив items
+        [self.items addObject:itemObj];
+        
+        if ([itemObj hasDiscount]) {
             [textRepresentation addObject:[itemObj itemDescription]];
         }
     }
@@ -61,6 +84,47 @@
     
     // выведем получившийся результат визуально
     self.resultLabel.text = resultString;
+}
+
+#pragma mark - UITableViewDataSource -
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // здесь необходимо указать количество элементов в секции таблицы
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // здесь описываем визуальное представление данных в ячейках таблицы
+    // введем переменную-идентификатор типа ячейки
+    NSString *cellIdentifier = @"cellIdentifier"; // имя типа произвольное
+    // здесь мы пробуем взять ячейку из очереди переиспользуемых дабы не создавать ее
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    // если ячеек в очереди нет, создаем ее
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    // далее добавим визуальное отображение данных
+    
+    // получим товар
+    Item *item = [self.items objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = item.title;
+    cell.detailTextLabel.text = [item itemDescription];
+    
+    // уберем подсветку выделенной ячейки
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    return cell;
+}
+
+// далее добавим реагирование на нажатие ячейки - этим занимается протокол UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // получаем выбранную ячейку
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    cell.accessoryType = (cell.accessoryType == UITableViewCellAccessoryCheckmark) ? UITableViewCellAccessoryNone : UITableViewCellAccessoryCheckmark;
 }
 
 @end
